04c6caf249f0231a5a3babbae8a90dda9755515d
Aralbek Muhammed <aralbekmuhammed.2007@gmail.com>
2023-06-14 12:14:31 +0600

    Update VideoView.swift

--- VideoView.swift
+++ VideoView.swift
@@ -1,58 +1,103 @@
+//
+//  VideoView.swift
+//  Test
+//
+//  Created by Muhammed Aralbek on 14.06.2023.
+//
+
 import UIKit
 import AVKit
-import AVFoundation
+import Combine
 
-class VideoView: UIView {
-    var playerLayer: AVPlayerLayer? = AVPlayerLayer()
-    var player: AVPlayer? = AVPlayer()
-    var isLoop: Bool = false
-    var rate: Float = 1
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+final class VideoView: UIView {
+    
+    private lazy var playerLayer: AVPlayerLayer = {
+        let layer = AVPlayerLayer()
+        layer.player = player
+        return layer
+    }()
+    
+    lazy var player: AVPlayer = {
+        let player = AVPlayer()
+        return player
+    }()
+    
+    var gravity: AVLayerVideoGravity {
+        get{
+            playerLayer.videoGravity
+        }set{
+            playerLayer.videoGravity = newValue
+        }
     }
-    private func updateFrames(){
-        playerLayer?.frame = layer.bounds
+    
+    var isLoop = false
+    
+    var speed: Float = 1 {
+        didSet{
+            if player.rate != 0{
+                player.rate = speed
+            }
+        }
     }
+    
+    @Published private(set) var isPlaying = false
+    
+    private var subscriptions = Set<AnyCancellable>()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupObservers()
+        layer.addSublayer(playerLayer)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func layoutSubviews() {
         super.layoutSubviews()
-        updateFrames()
+        playerLayer.frame = layer.bounds
     }
-    func configureVideo(withURL url: URL?, rate: Float = 1, loops: Bool = true, contentMode: AVLayerVideoGravity = .resizeAspect) {
-        if let url = url {
-            self.player?.replaceCurrentItem(with: AVPlayerItem(url: url))
-        }else{
-            self.player?.replaceCurrentItem(with: nil)
+    
+    func configure(with url: URL) {
+        if let currentItem = player.currentItem{
+            NotificationCenter.default.removeObserver(self, name: .AVPlayerItemDidPlayToEndTime, object: currentItem)
         }
-        self.rate = rate
-        self.isLoop = loops
-        playerLayer?.player = self.player
-        playerLayer?.videoGravity = contentMode 
-        if let playerLayer = self.playerLayer {
-            layer.sublayers?.removeAll(where: {$0 is AVPlayerLayer})
-            layer.addSublayer(playerLayer)
-        }
-        NotificationCenter.default.addObserver(self, selector: #selector(reachTheEndOfTheVideo(_:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: self.player?.currentItem)
+        
+        stop()
+        let item = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: item)
+        NotificationCenter.default.addObserver(self, selector: #selector(reachTheEndOfTheVideo(_:)),
+                                               name: .AVPlayerItemDidPlayToEndTime,
+                                               object: item)
     }
-    func isPlaying()->Bool{
-        player?.rate != 0 && player?.error == nil
-    }
+    
     func play() {
-        player?.play()
-        player?.rate = rate
+        player.play()
+        player.rate = speed
     }
+    
     func pause() {
-        player?.pause()
+        player.pause()
     }
+    
     func stop() {
-        player?.pause()
-        player?.seek(to: CMTime.zero)
+        player.pause()
+        player.seek(to: .zero)
     }
-    @objc func reachTheEndOfTheVideo(_ notification: Notification) {
+    
+    private func setupObservers(){
+        player.publisher(for: \.rate).sink { [weak self] rate in
+            self?.isPlaying = rate > 0
+        }.store(in: &subscriptions)
+    }
+    
+    @objc
+    private func reachTheEndOfTheVideo(_ notification: Notification) {
         if isLoop {
-            player?.pause()
-            player?.seek(to: CMTime.zero)
-            player?.play()
-            player?.rate = rate
+            stop()
+            play()
         }
     }
+    
 }
